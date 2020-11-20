deb7a7036d02fc8a7744b7b84b84ef12878abfbb
EthsonLiu <ethsonliu@outlook.com>
2020-11-20 17:01:29 +0800

    test/md5: add a test use case

--- bigmd5/bigmd5_test.go
+++ bigmd5/bigmd5_test.go
@@ -1,7 +1,20 @@
 package bigmd5
 
-import "testing"
+import (
+	"encoding/hex"
+	"testing"
+)
 
 func TestMD5File(t *testing.T) {
+	file := "../smallfile.txt"
+	b, err := MD5File(file)
+	if err != nil {
+		t.Errorf("%v\n", err)
+		return
+	}
 
+	if hex.EncodeToString(b) != "4dcaf0cc26cb2217f4e7cf5ca55dbe4b" {
+		t.Errorf("MD5File(%s) failed\n", file)
+		return
+	}
 }
